749381c774c6604e8e0b0773dd18fff7f4ddf22f
BeingSeight <ss9656484@gmail.com>
2024-11-19 15:21:22 +0530

    relational

--- Day2/Day2.3-Operators.cpp
+++ Day2/Day2.3-Operators.cpp
@@ -5,9 +5,19 @@ int main()
 {
 
              // ARITHMETIC
+             cout<<"ARITHMETIC"<<endl;
 cout<<4+5<<endl;
 cout<<4-5<<endl;
 cout<<4*5<<endl;
 cout<<5/3<<endl;
 cout<<5%3<<endl;
+
+             // RELATIONAL
+             cout<<"RELATIONAL"<<endl;
+cout<<(4>5)<<endl;
+cout<<(4<5)<<endl;
+cout<<(4>=5)<<endl;
+cout<<(4<=5)<<endl;
+cout<<(4==5)<<endl;
+cout<<(4!=5)<<endl;
 }
