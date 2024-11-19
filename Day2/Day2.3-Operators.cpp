2c352fb5eab8b577bb870b4a3586459efa423d86
BeingSeight <ss9656484@gmail.com>
2024-11-19 15:14:27 +0530

    operators

---
+++ Day2/Day2.3-Operators.cpp
@@ -0,0 +1,13 @@
+# include <iostream>
+using namespace std;
+
+int main()
+{
+
+             // ARITHMETIC
+cout<<4+5<<endl;
+cout<<4-5<<endl;
+cout<<4*5<<endl;
+cout<<5/3<<endl;
+cout<<5%3<<endl;
+}
