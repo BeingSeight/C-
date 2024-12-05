9a631d2ce895e758f4bf0e4cc886e68f12d29ac8
Sahil Shah <ss9656484@gmail.com>
2024-12-05 12:18:46 +0530

    default arg

--- Day5/default_arg.cpp
+++ Day5/default_arg.cpp
@@ -1,15 +1,16 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
+
+void SI(float p, float r, int n = 1)
+{
+    cout << "\nPrincipal Amount: " << p << endl;
+    cout << "Rate of Interest: " << r << endl;
+    n = n + 1;
+    cout << "\nSimple Interest: " << (p * r * n / 100) << endl;
+}
 
 int main()
 {
-    void SI(float p, float r, int n=1);
-
-    SI(10000, 8, 5, 2);
+    SI(10000, 8, 5);
     SI(15000, 9);
-}
-
-void SI(float p, float r, int n)
-{
-    cout<<"Simple Interest: "<<(p*r*n/100)<<endl;
 }
