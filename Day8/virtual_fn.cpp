0377cad58ea56d4cd584783ded6b8de22fb1d755
Sahil Shah <ss9656484@gmail.com>
2024-12-11 12:44:36 +0530

    virtual function

--- Day8/virtual_fn.cpp
+++ Day8/virtual_fn.cpp
@@ -4,18 +4,30 @@ using namespace std;
 class BaseClass{
 public:
 void method1(){
-    cout<<"Base class\n";
+    cout<<"\nBase class method1";
+}
+
+virtual void method2(){
+    cout<<"\nBase class method2";
 }
 };
 
-class DerivedClass:public BaseClass{
+class DerivedClass:public BaseClass
+{
 public:
 void method1(){
-    cout<<"Derived class\n";
+    cout<<"\nDerived class method1";
 }
-};
+void method2(){
+    cout<<"\nDerived class method2";
+}
 
+};
 int main(){
 DerivedClass obj;
-obj.method1();
+BaseClass *p0bj;
+p0bj=&obj;
+p0bj->method1();
+p0bj->method2();
+return 0;
 }
